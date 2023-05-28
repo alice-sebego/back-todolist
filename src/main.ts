639c1e7a08e442736d2e07e8a4df517ac430ad7f
Alice Sebego <alice.etvotrecom@gmail.com>
2023-05-28 20:41:16 +0200

    fix: change cdn's url of swagger css file for the swagger page

--- src/main.ts
+++ src/main.ts
@@ -41,9 +41,8 @@ async function bootstrap() {
         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
       ],
       customCssUrl: [
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0-alpha.14/swagger-ui.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0-alpha.14/swagger-ui.css',
       ],
     },
   });
